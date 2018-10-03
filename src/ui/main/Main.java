b38e44d075cc04fa81e4e3705063104ed6c57d9a
Luciano Souza <lucianosoaresdesouza@gmail.com>
2018-10-02 21:17:03 -0300

    Adicionando o main e principal

---
+++ src/ui/main/Main.java
@@ -0,0 +1,40 @@
+package ui.main;
+
+//https://stackoverflow.com/questions/36197764/javafx-stage-resizeable-false-and-maximized-true-hides-the-taskbar
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ * Classe principal do projeto
+ * @author Luciano
+ */
+public class Main extends Application {
+    
+    public void start(Stage stage) throws IOException{
+        
+        //Leitura do FXML
+        Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+        
+        //Criação da cena
+        Scene scene = new Scene(root);
+                
+        //Configuração da janela
+        stage.setScene(scene);
+        stage.show();
+        
+        
+    }
+    //Sempre fixo chamando a execucao do javafx
+    public static void main(String[] args) {
+        launch(args);
+    } 
+    
+}
