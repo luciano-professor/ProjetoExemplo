84b1e61b463974eeb7990b6e46b3941bc5004ae6
Luciano Souza <lucianosoaresdesouza@gmail.com>
2018-10-09 21:50:17 -0300

    Adicionando a logica do botao salvar produto

--- src/ui/produto/cadastro/ProdutoCadastroController.java
+++ src/ui/produto/cadastro/ProdutoCadastroController.java
@@ -5,9 +5,16 @@
  */
 package ui.produto.cadastro;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import model.entidades.Produto;
 
 /**
  * FXML Controller class
@@ -16,6 +23,25 @@ import javafx.fxml.Initializable;
  */
 public class ProdutoCadastroController implements Initializable {
 
+    @FXML
+    private JFXTextField id;
+    @FXML
+    private JFXTextField codigo;
+    @FXML
+    private JFXTextField nome;
+    @FXML
+    private JFXTextField preco;
+    @FXML
+    private JFXTextField quantidade;
+    @FXML
+    private JFXDatePicker validade;
+    @FXML
+    private JFXComboBox<String> comboBusca;
+    @FXML
+    private JFXTextField txtPesquisar;
+    @FXML
+    private TableView<Produto> tabela;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +49,27 @@ public class ProdutoCadastroController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    /**
+     * Salvar o produto no bd
+     * @param event 
+     */
+    @FXML
+    private void salvar(ActionEvent event) {
+        
+        double precoDouble = Double.parseDouble(preco.getText());
+        double qtdeDouble = Double.parseDouble(quantidade.getText());
+        
+        //Pegando os dados da tela e criando um produto
+        Produto p = new Produto(
+                0,
+                nome.getText(),
+                precoDouble,
+                codigo.getText(),
+                qtdeDouble,
+                validade.getValue()
+        );
+        
+    }
     
 }
